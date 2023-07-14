648ca5a8329186523cfe829d24591313e68cdc51
wilhelm nilede <wnilede@gmail.com>
2023-07-14 10:11:30 +0200

    Mayors have two votes.

--- lib/roles/villager.dart
+++ lib/roles/villager.dart
@@ -2,5 +2,14 @@ import 'package:vampulv/roles/role.dart';
 import 'package:vampulv/roles/role_type.dart';
 
 class Villager extends Role {
-  Villager() : super(type: RoleType.villager);
+  Villager()
+      : super(
+          type: RoleType.villager,
+          reactions: [
+            RoleReaction(
+              priority: 40,
+              onApply: (event, game, player) => player.roles.every((role) => role is Villager) ? player.copyWith(votesInLynching: 2) : null,
+            ),
+          ],
+        );
 }
